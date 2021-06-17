8c7c34551aa473dd7c4a68db190196db4413f888
KyDevtw <k3030789@gmail.com>
2021-06-17 13:37:51 +0800

    Cart Demo

--- src/ex-0617/5.實例-拆成左右兩個元件/src/App.js
+++ src/ex-0617/5.實例-拆成左右兩個元件/src/App.js
@@ -9,32 +9,18 @@ function App() {
   const [tshirtThree, setTshirtThree] = useState(1)
 
   // 代表每個產品的單價
-  const priceList = {
+  const price = {
     tshirtOne: 300,
     tshirtTwo: 200,
     tshirtThree: 450,
   }
 
-  // 每個產品的圖片
-  const pictureList = {
-    tshirtOne: 'https://i.imgur.com/1GrakTl.jpg"',
-    tshirtTwo: 'https://i.imgur.com/ba3tvGm.jpg',
-    tshirtThree: 'https://i.imgur.com/pHQ3xT3.jpg',
-  }
-
-  // 每個產品的名稱
-  const nameList = {
-    tshirtOne: '咖啡色 T-shirt',
-    tshirtTwo: '白色 T-shirt',
-    tshirtThree: '黑色 T-shirt',
-  }
-
   // 計算總價
   const total = () =>
     Number(
-      priceList['tshirtOne'] * tshirtOne +
-        priceList['tshirtTwo'] * tshirtTwo +
-        priceList['tshirtThree'] * tshirtThree
+      price['tshirtOne'] * tshirtOne +
+        price['tshirtTwo'] * tshirtTwo +
+        price['tshirtThree'] * tshirtThree
     ).toLocaleString()
 
   // 計算所有商品數量
@@ -51,9 +37,6 @@ function App() {
           setTshirtTwo={setTshirtTwo}
           tshirtThree={tshirtThree}
           setTshirtThree={setTshirtThree}
-          priceList={priceList}
-          pictureList={pictureList}
-          nameList={nameList}
         />
         <Summary
           productCount={productCount}
