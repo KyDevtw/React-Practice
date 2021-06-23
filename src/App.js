5e49d59b915c03ce0cea25bb7026f35603e9754e
KyDevtw <k3030789@gmail.com>
2021-06-23 12:06:52 +0800

    yarn add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import TodoApp from './components/TodoApp'
+
 
 function App() {
-  return (
-    <>
-      <TodoApp />
-    </>
-  )
+  return <></>
 }
 
 export default App
